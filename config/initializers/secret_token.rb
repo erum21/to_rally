23171efa9ff99dcf58b092d852ce8b02cb413bd6
Edwin Vega <edwin.ponc@gmail.com>
2010-10-24 17:37:24 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToRally::Application.config.secret_token = '936a1bed18265a4129748ad9c16d79d99f973525cfca7068bbb2a53aa1e97e3f65d9b6f0296a68d2d64998968777aca871b978a4a681c7e398005268053ffe7f'
